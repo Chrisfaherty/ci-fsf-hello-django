d623417f22631fd978550d4f7ea90c05c8c477b4
Chrisfaherty <chrismactom@gmail.com>
2023-01-12 13:32:37 +0000

    Update allowed host url

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-%wn18v5o7f0uy06!wd253
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = ['chrisfaherty-django-todo-app.herokuapp.com']
 
 
 # Application definition
