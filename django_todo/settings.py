b56f85371c9c38e1c08e07ec9ea16631584bb1cd
Chrisfaherty <chrismactom@gmail.com>
2023-01-12 13:38:03 +0000

    Add localhost to allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-%wn18v5o7f0uy06!wd253
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chrisfaherty-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
